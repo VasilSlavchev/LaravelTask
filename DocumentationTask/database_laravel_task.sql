27f92afce5cf30c3d2b1c38f2db1e6c88e052d6e
Vasil Slavchev <vasilslavchev@gmail.com>
2020-03-09 03:18:58 +0200

    Database, tables structure and relations.

---
+++ DocumentationTask/database_laravel_task.sql
@@ -0,0 +1,31 @@
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `jobs` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `job_title` varchar(80) NOT NULL,
+  `company_name` varchar(80) NOT NULL,
+  `start_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `and_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  FOREIGN KEY (candidate_id) REFERENCES candidates (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `candidates` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `first_name` varchar(80) NOT NULL,
+  `last_name` varchar(80) NOT NULL,
+  `email` varchar(80) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `job_title` varchar(80) NOT NULL,
+  `company_name` varchar(80) NOT NULL,
+  `start_date` datetime NOT NULL,
+  `and_date` datetime NOT NULL,
+  FOREIGN KEY (id) REFERENCES candidates (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
